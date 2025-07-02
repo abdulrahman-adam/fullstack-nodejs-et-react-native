bc300353d6353e1ce5efdb63228ae2f823721a84
abdulrahman <alshatyr@hotmail.com>
2025-07-02 16:27:06 +0200

    db modoles created

---
+++ backend/src/models/post.model.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+const postSchema = new Schema({
+    user: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
+    content: {type: String, maxLength: 280},
+    image: {type: String, default: ""},
+    likes: [
+        {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+    ],
+
+    comments: [
+        {type: mongoose.Schema.Types.ObjectId, ref: "Comment"},
+    ],
+
+}, {timestamps: true})
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
