ce0619b4e02721dc38ba1dd9d01429f1a70b1760
abdulrahman <alshatyr@hotmail.com>
2025-07-05 14:30:32 +0200

    user routes added

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,11 @@
+
+
+// This function for is the user authenticated or not
+
+export const protectRoute = async (req, res, next) => {
+
+    if(!req.auth().isAuthenticated) {
+        return res.status(401).json({message: "Unauthorized - you must be logged in"});
+    }
+    next();
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,28 @@
 import express from "express";
+import cors from "cors";
+import {clerkMiddleware} from "@clerk/express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 
+import userRoutes from "./routes/user.route.js"
 const app = express();
+
+
+// The middleware
+app.use(cors())
+app.use(express.json());
+
+// Handle the authentication
+app.use(clerkMiddleware());
+
+
 
 // Connect to Database
 // connectDB();
-app.get("/", (req, res) => res.send("✅ Hello from server"))
 
+// The routes
+app.get("/", (req, res) => res.send("✅ Hello from server"))
+app.use("/api/user", userRoutes);
 const startServer = async () => {
     try {
         // Connect to Database
